703a60350c69d1a8851f65048990b1257a8dd316
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-06-21 14:43:43 +0400

    using useRef for form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,27 @@
+import { useRef } from "react";
+
+//can use Ref when collecting form data and validating later all at once
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    nameInputRef.current.value = ''
+    //not ideal to manipulate the DOM this way
+  }
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
