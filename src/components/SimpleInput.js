0abe13b35f8d6a40f55472781f1f05cab09ca23a
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-06-21 14:33:17 +0400

    added useState version

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('')
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(name)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={handleChange} value={name} />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
