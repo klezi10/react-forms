a259498786ce271c35fe874cac17801bf8f3ba2f
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-06-22 13:14:31 +0400

    added basic form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,23 +3,33 @@ import { useState } from "react";
 //can use useState to validate on each keystroke
 
 const SimpleInput = (props) => {
-  const [name, setName] = useState('')
+  const [name, setName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(true);
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
 
   function handleSubmit(event) {
-    event.preventDefault()
-    console.log(name)
-    setName('')
+    event.preventDefault();
+
+    if (name.trim() === "") {
+      setNameIsValid(false);
+      return;
+    }
+    setNameIsValid(true);
+    console.log(name);
+    setName("");
   }
+
+  const nameInputClass = nameIsValid ? "form-control" : "form-control invalid"
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={handleChange} value={name} />
+      <div className={nameInputClass}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={handleChange} value={name} />
+        {!nameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button type="submit">Submit</button>
